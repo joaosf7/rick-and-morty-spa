35463ea5e84a85112176c07a3983235b968d1d24
joaosf <joaosf7@gmail.com>
2024-02-18 21:14:17 +0000

    Removes spots option

--- app/view/components/navbar.js
+++ app/view/components/navbar.js
@@ -8,7 +8,6 @@ export default function render(){
                 <ul id='dropdown-list' class="dropdown-menu" aria-labelledby="navbar-button">
                     <li><a class="dropdown-item" href="#episodes">Episodes</a></li>
                     <li><a class="dropdown-item" href="#characters">Characters</a></li>
-                    <li><a class="dropdown-item" href="#spots">Spots</a></li>
                 </ul>
             </div>
             <h3 id='navbar-title'>Rick and Morty Rabbit Hole</h3>
